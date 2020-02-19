275c1f8bfea3157ab19589ca0fccdf06be1f85b8
Matthew Davis <matthew@matthewdavis.io>
2020-02-19 00:47:47 -0500

    db

---
+++ src/db/DBOptions.ts
@@ -0,0 +1,8 @@
+import { EntitySchema } from 'typeorm';
+
+export class DBOptions {
+
+    public synchronize?: boolean;
+    public entities?: Array<Function | string | EntitySchema<any>>;
+
+}
---
+++ src/db/DBUtilities.ts
@@ -0,0 +1,25 @@
+import { DynamicModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DBOptions }     from './DBOptions';
+
+export class DBUtilities {
+
+    public static getModule(dbOptions: DBOptions): DynamicModule {
+
+        return TypeOrmModule.forRoot({
+
+            type: 'mysql',
+            host: process.env.DB_HOSTNAME || 'localhost',
+            port: Number.parseInt(process.env.DB_PORT) || 3306,
+            username: process.env.DB_USERNAME || 'root',
+            password: process.env.DB_PASSWORD || 'mysql',
+            database: process.env.DB_NAME || 'nestjs',
+            synchronize: process.env.DB_SYNCHRONIZE === 'true',
+            keepConnectionAlive: true,
+            entities: dbOptions.entities
+
+        });
+
+    }
+
+}
