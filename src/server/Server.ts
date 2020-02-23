2f4ac5cc0d2c00a614e4ef9a8b7e4911e9afb7a7
Matthew Davis <matthew@matthewdavis.io>
2020-02-22 22:22:21 -0500

    env

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -62,17 +62,19 @@ export class Server {
 
     public static getEnvironment(): 'local' | 'docker' | 'k8' {
 
-        if (fs.existsSync('/proc/1/cgroups')) {
+        if (fs.existsSync('/proc/1/cgroup')) {
 
-            const contents = fs.readFileSync('/proc/1/cgroups');
+            const contents = fs.readFileSync('/proc/1/cgroup');
 
             if (contents.indexOf('docker') > -1) {
 
                 return 'docker';
 
-            }
+            } else if (contents.indexOf('kube') > -1) {
 
-            return 'k8';
+                return 'k8';
+
+            }
 
         }
 
