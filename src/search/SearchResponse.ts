5791319f9f908bdf997a11db35064db8c25833bb
Matthew Davis <matthew@matthewdavis.io>
2021-04-08 18:45:18 -0400

    bump :pray:

--- src/search/SearchResponse.ts
+++ src/search/SearchResponse.ts
@@ -5,7 +5,11 @@ import { SearchResponseGenerator } from './SearchResponseGenerator';
 import { SearchResponseOptions } from './SearchResponseOptions';
 
 export const SearchResponse = (options: SearchResponseOptions) => applyDecorators(
-    ApiQuery({ name: 'condition', type: SearchCondition }),
+    ApiQuery({
+        name: 'condition',
+        isArray: true,
+        type: SearchCondition
+    }),
     ApiQuery({ name: 'limit', type: Number }),
     ApiQuery({ name: 'page', type: Number }),
     SearchResponseGenerator(options.type)
