5bb7c50f5a22ff1888a7fc66860f270743acfb59
Matthew Davis <matthew@matthewdavis.io>
2021-04-08 18:55:26 -0400

    bump :pray:

--- src/search/SearchRegister.ts
+++ src/search/SearchRegister.ts
@@ -1,15 +1,28 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiQuery } from '@nestjs/swagger';
-import { SearchCondition } from './SearchCondition';
 import { SearchResponseGenerator } from './SearchResponseGenerator';
 import { SearchResponseOptions } from './SearchResponseOptions';
 
-export const SearchResponse = (options: SearchResponseOptions) => applyDecorators(
+export const SearchRegister = (options: SearchResponseOptions) => applyDecorators(
     ApiQuery({
         name: 'condition',
         isArray: true,
-        type: SearchCondition,
-        explode: true
+        explode: false,
+        schema: {
+
+            type: 'object',
+            properties: {
+
+                condition: {
+
+                    type: 'string'
+
+                }
+
+            }
+
+        }
+        
     }),
     ApiQuery({ name: 'limit', type: Number }),
     ApiQuery({ name: 'page', type: Number }),
