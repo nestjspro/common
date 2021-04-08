a6e5f9626ffa86208e62cb64a737f51f096bc7db
Matthew Davis <matthew@matthewdavis.io>
2021-04-08 17:11:09 -0400

    bump :pray:

--- src/search/SearchCondition.ts
+++ src/search/SearchCondition.ts
@@ -1,10 +1,16 @@
 import { SearchOperator } from './SearchOperator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SearchCondition {
 
-    condition: string;
-    parameters?: { [ name: string ]: string };
-    operator?: SearchOperator;
+    @ApiProperty({ description: 'Condition(s) to match.', example: 'name = :name' })
+    public condition: string;
+
+    @ApiProperty({ description: 'Parameters(s) to match.', example: '{ name: "John" }' })
+    public parameters?: { [ name: string ]: string };
+
+    @ApiProperty({ description: 'Operator to use when there are multiple conditions', enum: SearchOperator })
+    public operator?: SearchOperator;
 
     public constructor(condition: string, parameters: { [ name: string ]: string }, operator?: SearchOperator) {
 
