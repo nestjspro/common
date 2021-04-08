bf0a2eff093bc9972f37afce9ddedb7565232106
Matthew Davis <matthew@matthewdavis.io>
2021-04-08 16:14:02 -0400

    bump :pray:

--- src/search/SearchConditionEquals.ts
+++ src/search/SearchConditionEquals.ts
@@ -1,9 +1,8 @@
 import { SearchConditionBase } from './SearchConditionBase';
-import { SearchOperator } from './SearchOperator';
 
 export class SearchConditionEquals extends SearchConditionBase {
 
-    public constructor(column: string, value: any, operator: SearchOperator) {
+    public constructor(column: string, value: any) {
 
         super(column, value, 'equals');
 
