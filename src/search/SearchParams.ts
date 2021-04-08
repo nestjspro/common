d72a37e42e5eda0b74c694a3db6293fda115ba7a
Matthew Davis <matthew@matthewdavis.io>
2021-04-08 19:40:35 -0400

    bump :pray:

--- src/search/SearchParams.ts
+++ src/search/SearchParams.ts
@@ -1,33 +1,9 @@
 import { BadRequestException, createParamDecorator } from '@nestjs/common';
 import { SearchCondition } from './SearchCondition';
-import { PaginationSortDirection } from './PaginationSort';
 
 export const SearchParams: () => ParameterDecorator = createParamDecorator((data, req) => {
 
-    const { limit = 20, page = 1, sort, condition, parameter, operator } = req.query;
-
-    let sortColumn;
-    let sortDirection = PaginationSortDirection.ASC;
-
-    if (sort) {
-
-        const split = sort.split(',');
-
-        if (split.length == 1) {
-
-            sortColumn = split[ 0 ];
-
-        } else if (split.length == 2) {
-
-            [ sortColumn, sortDirection ] = split;
-
-        } else {
-
-            throw new BadRequestException('invalid sort');
-
-        }
-
-    }
+    const { limit = 20, page = 1, sort, direction, condition, parameter, operator } = req.query;
 
     if (operator) {
 
@@ -60,7 +36,7 @@ export const SearchParams: () => ParameterDecorator = createParamDecorator((data
 
             limit, page, sort: {
 
-                [ sortColumn ]: sortDirection
+                [ sort ]: direction
 
             }
 
