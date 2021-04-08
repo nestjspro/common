c9eebbd7b01755fe48f6f4ddf37a0f5adda55d61
Matthew Davis <matthew@matthewdavis.io>
2021-04-08 00:37:59 -0400

    bump :pray:

--- src/search/Search.ts
+++ src/search/Search.ts
@@ -2,10 +2,10 @@ import { PaginationOptions } from './PaginationOptions';
 import { SearchCondition } from './SearchCondition';
 import { SearchOperator } from './SearchOperator';
 
-export interface Search<T> {
+export class Search<T> {
 
     conditions: Array<SearchCondition>;
     options?: PaginationOptions;
-    operator?: SearchOperator
+    operator?: SearchOperator;
 
 }
