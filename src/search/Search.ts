05805c01c9d71ab0d6cfd867ac3d365197310649
Matthew Davis <matthew@matthewdavis.io>
2021-04-08 01:01:29 -0400

    bump :pray:

--- src/search/Search.ts
+++ src/search/Search.ts
@@ -1,11 +1,9 @@
 import { PaginationOptions } from './PaginationOptions';
 import { SearchCondition } from './SearchCondition';
-import { SearchOperator } from './SearchOperator';
 
 export class Search<T> {
 
     conditions: Array<SearchCondition>;
     options?: PaginationOptions;
-    operator?: SearchOperator;
 
 }
